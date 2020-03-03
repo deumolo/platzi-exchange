5b756cec7377af7cf692caff56d36c20f64a5db9
Daniel Moreno <deumolo@gmail.com>
2020-03-03 15:29:23 -0600

    Project kick off

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '@/assets/css/tailwind.css' //@ es source
+import router from '@/router'
+import { dollarFilter } from '@/filters'
+import { percentFilter } from '@/filters'
+import Chartkick from 'vue-chartkick'
+import { VueSpinners } from '@saeris/vue-spinners'
+import Chart from 'chart.js'
+
+Vue.use(VueSpinners)
+Vue.use(Chartkick.use(Chart))
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
+Vue.config.productionTip = false
+
+new Vue({
+  router: router,
+  render: h => h(App)
+}).$mount('#app')
